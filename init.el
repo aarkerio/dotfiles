e0b9944c0bf6d97d9b01f5189a81c8b3f41e2291
aarkerio <mmontoya@gmail.com>
2017-10-31 23:27:37 -0600

    hhh

--- init.el
+++ init.el
@@ -166,6 +166,20 @@
   (progn
     (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)))
 
+(use-package company
+  :ensure t
+  :defer t
+  :init (global-company-mode)
+  :config
+  (progn
+    ;; Use Company for completion
+    (bind-key [remap completion-at-point] #'company-complete company-mode-map)
+    (setq company-tooltip-align-annotations t
+          ;; Easy navigation to candidates with M-<n>
+          company-show-numbers t)
+    (setq company-dabbrev-downcase nil))
+  :diminish company-mode)
+
 ;; (use-package paredit
 ;;   :ensure t
 ;;   :init
