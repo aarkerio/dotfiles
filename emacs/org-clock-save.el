7ec7b6c3eca15f6167e19215129ca5669c5e9b7c
aarkerio <mmontoya@gmail.com>
2020-08-04 18:55:49 -0500

    Adding snippets

--- emacs/org-clock-save.el
+++ emacs/org-clock-save.el
@@ -1,2 +1,2 @@
-;; org-clock-save.el - pav23 at <2020-08-02 So 14:54>
+;; org-clock-save.el - pav23 at <2020-08-03 Mo 20:47>
 (setq org-clock-stored-history '(("/home/manuel/entwicklung/chipotle/rdigital/notes/resolvedigi.org" . 167) ("/home/manuel/entwicklung/chipotle/rdigital/notes/resolvedigi.org" . 245)))
