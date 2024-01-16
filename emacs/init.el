b2bef68154ad3a8e619f374088c85d63b0432e84
aarkerio <mmontoya@gmail.com>
2024-01-16 11:28:53 -0600

    Upddate emacs

--- emacs/init.el
+++ emacs/init.el
@@ -237,7 +237,7 @@
 
 (use-package ace-jump-buffer
   :bind
-  ("s-|" . ace-jump-buffer)
+  ("s-q" . ace-jump-buffer)
   :config
   (make-ace-jump-buffer-function
       "special"
@@ -482,6 +482,12 @@
            yaml-jsyaml))
   :config (global-flycheck-mode))
 
+(use-package flycheck-eglot
+  :ensure t
+  :after (flycheck eglot)
+  :config
+  (global-flycheck-eglot-mode 1))
+
 (use-package flycheck-clojure
   :ensure t
   :commands (flycheck-clojure-setup)               ;; autoload
@@ -502,22 +508,37 @@
   :config
   (with-eval-after-load 'flycheck (flycheck-pos-tip-mode)))
 
+(use-package all-the-icons-ivy
+  :init (add-hook 'after-init-hook 'all-the-icons-ivy-setup))
+
+(use-package all-the-icons-ivy-rich
+  :ensure t
+  :init (all-the-icons-ivy-rich-mode 1))
+
+(use-package ivy-rich
+  :ensure t
+  :init (ivy-rich-mode 1))
+
+(use-package swiper
+  :ensure t )
+
 (use-package frog-jump-buffer
   :ensure t
   :bind
-  (([(?\s-<)] . frog-jump-buffer))
+  (([(?\s-w)] . frog-jump-buffer))
 	:init
-	(setq frog-jump-buffer-posframe-parameters '((left-fringe . 5)
+	((setq frog-jump-buffer-posframe-parameters '((left-fringe . 5)
 																							 (right-fringe . 5)
 																							 (internal-border-width . 5)
 																							 (internal-border . "#6ba499")
 																							 (internal-border-color . "#6ba499")
 																							 (background-color . "#eee8d5")
                                                (background . "#eee8d5")))
+	 (setq frog-jump-buffer-use-all-the-icons-ivy t))
+
   :config
   (progn
-    (dolist (regexp '("TAGS" "^\\*Compile-log" "-debug\\*$" "^\\:" "helm\\*$" "errors\\*$" "^\\*Backtrace" "-ls\\*$" "^\\*dashboard"
-                  "stderr\\*$" "^\\*Flymake" "^\\*vc" "^\\*Warnings" "^\\*Messages" "*cider-error*" "^\\*helm" "^\\*magit" "^\\*eldoc" "\\^*Shell Command"))
+    (dolist (regexp '("TAGS" "^\\*Compile-log" "-debug\\*$" "^\\:" "*tide-server*" "helm\\*$" "errors\\*$" "^\\*Backtrace" "-ls\\*$" "^\\*dashboard" "stderr\\*$" "^\\*Async" "^\\*Flymake" "^\\*vc" "^\\*Warnings" "^\\*Messages" "*cider-error*" "^\\*helm" "^\\*magit" "^\\*eldoc" "\\^*Shell Command"))
     (push regexp frog-jump-buffer-ignore-buffers))))
 
 (use-package git-timemachine
@@ -958,7 +979,7 @@
  '(cua-overwrite-cursor-color "#b58900")
  '(cua-read-only-cursor-color "#859900")
  '(custom-safe-themes
-   '("c433c87bd4b64b8ba9890e8ed64597ea0f8eb0396f4c9a9e01bd20a04d15d358" "eeb23ebf4a97b95a85f6f5e6b8524a9854da008f494828f0e78693675d6fc9ca" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default))
+	 '("c433c87bd4b64b8ba9890e8ed64597ea0f8eb0396f4c9a9e01bd20a04d15d358" "eeb23ebf4a97b95a85f6f5e6b8524a9854da008f494828f0e78693675d6fc9ca" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default))
  '(display-fill-column-indicator t)
  '(fci-rule-color "#eee8d5")
  '(fill-column 100)
@@ -968,28 +989,28 @@
  '(helm-ff-lynx-style-map t)
  '(highlight-changes-colors '("#d33682" "#6c71c4"))
  '(highlight-symbol-colors
-   '("#efe4da49afb1" "#cfc4e1acd08b" "#fe52c9e6b34e" "#dbb6d3c2dcf3" "#e183dee0b053" "#f944cc6dae47" "#d35fdac4e069"))
+	 '("#efe4da49afb1" "#cfc4e1acd08b" "#fe52c9e6b34e" "#dbb6d3c2dcf3" "#e183dee0b053" "#f944cc6dae47" "#d35fdac4e069"))
  '(highlight-symbol-foreground-color "#586e75")
  '(highlight-tail-colors
-   '(("#eee8d5" . 0)
-     ("#b3c34d" . 20)
-     ("#6ccec0" . 30)
-     ("#74adf5" . 50)
-     ("#e1af4b" . 60)
-     ("#fb7640" . 70)
-     ("#ff699e" . 85)
-     ("#eee8d5" . 100)))
+	 '(("#eee8d5" . 0)
+		 ("#b3c34d" . 20)
+		 ("#6ccec0" . 30)
+		 ("#74adf5" . 50)
+		 ("#e1af4b" . 60)
+		 ("#fb7640" . 70)
+		 ("#ff699e" . 85)
+		 ("#eee8d5" . 100)))
  '(hl-bg-colors
-   '("#e1af4b" "#fb7640" "#ff6849" "#ff699e" "#8d85e7" "#74adf5" "#6ccec0" "#b3c34d"))
+	 '("#e1af4b" "#fb7640" "#ff6849" "#ff699e" "#8d85e7" "#74adf5" "#6ccec0" "#b3c34d"))
  '(hl-fg-colors
-   '("#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3"))
+	 '("#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3"))
  '(hl-paren-colors '("#2aa198" "#b58900" "#268bd2" "#6c71c4" "#859900"))
  '(js2-include-node-externs t)
  '(nrepl-message-colors
-   '("#dc322f" "#cb4b16" "#b58900" "#5b7300" "#b3c34d" "#0061a8" "#2aa198" "#d33682" "#6c71c4"))
+	 '("#dc322f" "#cb4b16" "#b58900" "#5b7300" "#b3c34d" "#0061a8" "#2aa198" "#d33682" "#6c71c4"))
  '(org-agenda-files nil)
  '(package-selected-packages
-   '(rspec-mode dir-treeview-themes material-light cape corfu tab-bar-buffers lsp-treemacs all-the-icons-completion all-the-icons-dired all-the-icons-ibuffer nose highlight-indentation lsp-ui quelpa bookmark-view bm vdiff efar rvm smooth-scrolling color-theme-sanityinc-solarized nurumacs dired-subtree dired-icon vue-html-mode mmm-mode lsp-mode doom-themes eglot posframe pug-mode vue-mode rubocopfmt rubocop slim-mode jekyll-modes easy-jekyll coffee-mode comint-better-defaults esh-autosuggest eshell-prompt-extras cider ac-cider anakondo haml-mode flymake-haml modus-operandi-theme flycheck-clj-kondo helm-ag prettier-js rjsx-mode alect-themes apropospriate-theme anti-zenburn-theme ahungry-theme ace-jump-buffer better-jumper yaml-mode web-mode use-package-chords undo-tree transpose-frame tide tabbar solarized-theme smart-mode-line-powerline-theme rainbow-delimiters projectile popwin parseclj org-bullets neotree multiple-cursors markdown-mode majapahit-theme magit json-mode js2-mode ivy imenu-anywhere helm graphql-mode go-direx git-timemachine flycheck-pos-tip flycheck-clojure exec-path-from-shell discover dired-quick-sort dashboard company col-highlight clojurescript-mode clojure-snippets buffer-flip avy auctex all-the-icons))
+	 '(swiper all-the-icons-ivy-rich rspec-mode dir-treeview-themes material-light cape corfu tab-bar-buffers lsp-treemacs all-the-icons-completion all-the-icons-dired all-the-icons-ibuffer nose highlight-indentation lsp-ui quelpa bookmark-view bm vdiff efar rvm smooth-scrolling color-theme-sanityinc-solarized nurumacs dired-subtree dired-icon vue-html-mode mmm-mode lsp-mode doom-themes eglot posframe pug-mode vue-mode rubocopfmt rubocop slim-mode jekyll-modes easy-jekyll coffee-mode comint-better-defaults esh-autosuggest eshell-prompt-extras cider ac-cider anakondo haml-mode flymake-haml modus-operandi-theme flycheck-clj-kondo helm-ag prettier-js rjsx-mode alect-themes apropospriate-theme anti-zenburn-theme ahungry-theme ace-jump-buffer better-jumper yaml-mode web-mode use-package-chords undo-tree transpose-frame tide tabbar solarized-theme smart-mode-line-powerline-theme rainbow-delimiters projectile popwin parseclj org-bullets neotree multiple-cursors markdown-mode majapahit-theme magit json-mode js2-mode ivy imenu-anywhere helm graphql-mode go-direx git-timemachine flycheck-pos-tip flycheck-clojure exec-path-from-shell discover dired-quick-sort dashboard company col-highlight clojurescript-mode clojure-snippets buffer-flip avy auctex all-the-icons))
  '(pos-tip-background-color "#eee8d5")
  '(pos-tip-foreground-color "#586e75")
  '(powerline-default-separator 'curve)
@@ -1004,28 +1025,28 @@
  '(vc-annotate-background nil)
  '(vc-annotate-background-mode nil)
  '(vc-annotate-color-map
-   '((20 . "#dc322f")
-     (40 . "#cb4366eb20b4")
-     (60 . "#c1167942154f")
-     (80 . "#b58900")
-     (100 . "#a6ae8f7c0000")
-     (120 . "#9ed892380000")
-     (140 . "#96be94cf0000")
-     (160 . "#8e5397440000")
-     (180 . "#859900")
-     (200 . "#77679bfc4635")
-     (220 . "#6d449d465bfd")
-     (240 . "#5fc09ea47092")
-     (260 . "#4c68a01784aa")
-     (280 . "#2aa198")
-     (300 . "#303498e7affc")
-     (320 . "#2fa1947cbb9b")
-     (340 . "#2c879008c736")
-     (360 . "#268bd2")))
+	 '((20 . "#dc322f")
+		 (40 . "#cb4366eb20b4")
+		 (60 . "#c1167942154f")
+		 (80 . "#b58900")
+		 (100 . "#a6ae8f7c0000")
+		 (120 . "#9ed892380000")
+		 (140 . "#96be94cf0000")
+		 (160 . "#8e5397440000")
+		 (180 . "#859900")
+		 (200 . "#77679bfc4635")
+		 (220 . "#6d449d465bfd")
+		 (240 . "#5fc09ea47092")
+		 (260 . "#4c68a01784aa")
+		 (280 . "#2aa198")
+		 (300 . "#303498e7affc")
+		 (320 . "#2fa1947cbb9b")
+		 (340 . "#2c879008c736")
+		 (360 . "#268bd2")))
  '(vc-annotate-very-old-color nil)
  '(warning-suppress-types '((use-package) (use-package) (use-package)))
  '(weechat-color-list
-   '(unspecified "#fdf6e3" "#eee8d5" "#a7020a" "#dc322f" "#5b7300" "#859900" "#866300" "#b58900" "#0061a8" "#268bd2" "#a00559" "#d33682" "#007d76" "#2aa198" "#657b83" "#839496")))
+	 '(unspecified "#fdf6e3" "#eee8d5" "#a7020a" "#dc322f" "#5b7300" "#859900" "#866300" "#b58900" "#0061a8" "#268bd2" "#a00559" "#d33682" "#007d76" "#2aa198" "#657b83" "#839496")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -1201,7 +1222,7 @@
 (add-hook 'ruby-mode-hook
   (lambda ()
     (setq-local flycheck-command-wrapper-function
-                (lambda (command) (append '("/home/xpsman/.rvm/gems/default/bin/bundle" "exec") command)))))
+                (lambda (command) (append '("/home/xpsman/.rvm/rubies/default/bin/bundle" "exec") command)))))
 
 (autoload 'ruby-mode "ruby-mode" "Major mode for ruby files" t)
 (add-to-list 'auto-mode-alist '("\\.rb\\'" . ruby-mode))
